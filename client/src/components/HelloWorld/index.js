dbfc39405114554594029fc9e0a54eb3c5bb3cb0
spidercatnat <natalieb@hey.com>
2020-10-13 00:09:29 -0700

    Adds class notation example for example component

--- client/src/components/HelloWorld/index.js
+++ client/src/components/HelloWorld/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import { subscribe } from "../../pubsub";
 
 export const HelloWorld = props => subscribe(({ topic, data }) => {
-    return <span>{data || ""} | {props.passedDownFromProvider} <br /> I am listening to the {topic} topic. </span>
+    return <span>{data} | {props.passedDownFromProvider} <br /> I am listening to the {topic} topic. </span>
 }, props.topic);
 
+
+// Or, alternatively, using class notation
+//
+// class HelloWorld extends Component {
+//     render() {
+//         const { props: { passedDownFromProvider, topic } } = this;
+//         return subscribe(({ data }) => {
+//             return <span>{data} | {passedDownFromProvider} <br /> I am listening to the {topic} topic. </span>
+//         }, topic)
+//     }
+// }
+
+// export { HelloWorld }
